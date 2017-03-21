52d21d3fb79ccb1088fe4f7e3fea2d915c85c836
Raphael <rapssail@hotmail.fr>
2017-03-21 19:33:55 +0100

    fix indentation

--- main.py
+++ main.py
@@ -25,10 +25,9 @@ digits = datasets.load_digits()
 
 @app.route('/')
 def calculate():
-	clf = svm.SVC(gamma=0.001, C=100.)
+    clf = svm.SVC(gamma=0.001, C=100.)
     clf.fit(digits.data[:-1], digits.target[:-1])
     prediction = clf.predict(digits.data[-1:])
-
     return jsonify({'prediction': repr(prediction)})
 
 
